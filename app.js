a0e4d2b35f223ae68c58a13636500935b94afd0b
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-03-27 18:37:39 +0100

    Mocking request

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var xmlrpc         = require('xmlrpc');
+var xmlrpc = require('xmlrpc');
 var session = require('express-session')
 
 var app = express();
@@ -17,22 +17,26 @@ var datasets = {};
 
 var router = express.Router();
 
-xmlrpc_host = { host: 'deepblue.mpi-inf.mpg.de', port: 80, path: '/xmlrpc'};
+xmlrpc_host = {
+  host: 'deepblue.mpi-inf.mpg.de',
+  port: 80,
+  path: '/xmlrpc'
+};
 //xmlrpc_host = { host: '127.0.0.1', port: 31415, path: '/'};
 var client = xmlrpc.createClient(xmlrpc_host);
 
-var Command = function (name, parameters) {
+var Command = function(name, parameters) {
   var self = this;
   self.name = name;
   self.parameters = parameters;
 
-  self.convert = function (value, type) {
+  self.convert = function(value, type) {
     if (type == "string") {
       return valeue;
     }
   }
 
-  self.doRequest = function (req, res) {
+  self.doRequest = function(req, res) {
     console.log(self);
     console.log("executing : " + name + " " + parameters);
     console.log(req.query);
@@ -50,12 +54,11 @@ var Command = function (name, parameters) {
         var raw_value = req.query[parameter_name];
         if (parameter_type == "string") {
           xmlrpc_request_parameters.push(raw_value);
-        }
-         else if (parameter_type == "int") {
+        } else if (parameter_type == "int") {
           xmlrpc_request_parameters.push(parseInt(raw_value));
         } else if (parameter_type == "double") {
-         xmlrpc_request_parameters.push(parseFloat(raw_value));
-        } else  {
+          xmlrpc_request_parameters.push(parseFloat(raw_value));
+        } else {
           res.send("Internal error: Unknown variables type " + parameter_type);
           return;
         }
@@ -66,7 +69,7 @@ var Command = function (name, parameters) {
     console.log(xmlrpc_request_parameters);
 
     client.methodCall(name, xmlrpc_request_parameters, function(error, value) {
-     if (error) {
+      if (error) {
         console.log('error:', error);
         console.log('req headers:', error.req && error.req._header);
         console.log('res code:', error.res && error.res.statusCode);
@@ -79,7 +82,7 @@ var Command = function (name, parameters) {
 }
 
 
-client.methodCall('commands', [], function (error, value) {
+client.methodCall('commands', [], function(error, value) {
   if (error) {
     console.log('error:', error);
     console.log('req headers:', error.req && error.req._header);
@@ -93,15 +96,15 @@ client.methodCall('commands', [], function (error, value) {
     var commands = value[1];
     for (var command_name in commands) {
       var command = new Command(command_name, commands[command_name]["parameters"]);
-      router.get("/"+command_name, command.doRequest);
+      router.get("/" + command_name, command.doRequest);
     }
   }
 });
 
 
-var status = function (req, res) {
+var status = function(req, res) {
   var client = xmlrpc.createClient(xmlrpc_host);
-  client.methodCall('getStatus', [], function (error, value) {
+  client.methodCall('getStatus', [], function(error, value) {
     if (error) {
       console.log('error:', error);
       console.log('req headers:', error.req && error.req._header);
@@ -113,11 +116,63 @@ var status = function (req, res) {
   })
 };
 
+/*** ****/
+
+var get_data = function(collection, columns, start, length, global_search, sort_column, sort_direction) {
+
+  console.log("get_data");
+  return {};
+}
+
+
+
+/***  ****/
+
+
 // TODO: datatables :)
-var datatable = function (req, res) {
+var datatable = function(req, res) {
   console.log("Datatable");
-  console.log(req);
-  res.send(":)");
+  console.log(req.query);
+
+  var collection = req.query.collection;
+
+  var columns = [];
+  var columns_count = parseInt(req.query.iColumns);
+  for (var i = 0; i < columns_count; i++) {
+    var param_name = "col_" + i.toString();
+    var column_name = req.query[param_name];
+    columns.push(column_name);
+  }
+  console.log(columns);
+
+  var start = parseInt(req.query.iDisplayStart);
+  console.log(start);
+  var length = parseInt(req.query.iDisplayLength);
+  console.log(length);
+
+  console.log("global_search");
+  var global_search = req.query.sSearch;
+  console.log(global_search);
+
+  console.log("sort_column");
+  var sort_column = req.query.iSortCol_0;
+  console.log(sort_column);
+
+  console.log("sort_direction");
+  var sort_direction = req.query.sSortDir_0;
+  console.log(sort_direction);
+
+  var result = get_data(collection, columns, start, length, global_search, sort_column, sort_direction);
+
+  result.sEcho = req.query.sEcho;
+
+  result.iTotalRecords = 0;
+  result.iTotalDisplayRecords = 0;
+  result.aaData = [];
+
+  console.log(result);
+
+  res.send(result);
 };
 
 router.get('/status', status);
@@ -130,14 +185,16 @@ app.use('/', router);
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -145,23 +202,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
