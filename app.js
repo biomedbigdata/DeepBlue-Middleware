2f0d64d2ab582deb4211a3fb11470e6536abf1fa
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-04-07 12:51:35 +0200

    Moving the REST to XML-RPC to its own module

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var xmlrpc = require('xmlrpc');
-var session = require('express-session')
+var session = require('express-session');
+
+var deepblue_cache = require('./cache');
+var rest_to_xmlrpc = require('./rest_to_xmlrpc');
+var settings = require('./settings');
 
 var app = express();
+
+var router = rest_to_xmlrpc.router;
 
 app.use(session({
   resave: false,
@@ -14,92 +20,6 @@ app.use(session({
 }))
 
 var datasets = {};
-
-var router = express.Router();
-
-xmlrpc_host = {
-  host: 'deepblue.mpi-inf.mpg.de',
-  port: 80,
-  path: '/xmlrpc'
-};
-//xmlrpc_host = { host: '127.0.0.1', port: 31415, path: '/'};
-var client = xmlrpc.createClient(xmlrpc_host);
-
-var Command = function(name, parameters) {
-  var self = this;
-  self.name = name;
-  self.parameters = parameters;
-
-  self.convert = function(value, type) {
-    if (type == "string") {
-      return valeue;
-    }
-  }
-
-  self.doRequest = function(req, res) {
-    console.log(self);
-    console.log("executing : " + name + " " + parameters);
-    console.log(req.query);
-
-    var xmlrpc_request_parameters = [];
-
-    for (pos in self.parameters) {
-      var parameter = self.parameters[pos];
-      var parameter_name = parameter[0];
-      var parameter_type = parameter[1];
-      var multiple = parameter[2];
-      console.log(req.query);
-      console.log("param: " + req.query[parameter_name] + " type " + parameter_type);
-      if (parameter_name in req.query) {
-        var raw_value = req.query[parameter_name];
-        if (parameter_type == "string") {
-          xmlrpc_request_parameters.push(raw_value);
-        } else if (parameter_type == "int") {
-          xmlrpc_request_parameters.push(parseInt(raw_value));
-        } else if (parameter_type == "double") {
-          xmlrpc_request_parameters.push(parseFloat(raw_value));
-        } else {
-          res.send("Internal error: Unknown variables type " + parameter_type);
-          return;
-        }
-      } else {
-        xmlrpc_request_parameters.push(null);
-      }
-    }
-    console.log(xmlrpc_request_parameters);
-
-    client.methodCall(name, xmlrpc_request_parameters, function(error, value) {
-      if (error) {
-        console.log('error:', error);
-        console.log('req headers:', error.req && error.req._header);
-        console.log('res code:', error.res && error.res.statusCode);
-        console.log('res body:', error.body);
-      } else {
-        res.send(value);
-      }
-    });
-  }
-}
-
-
-client.methodCall('commands', [], function(error, value) {
-  if (error) {
-    console.log('error:', error);
-    console.log('req headers:', error.req && error.req._header);
-    console.log('res code:', error.res && error.res.statusCode);
-    console.log('res body:', error.body);
-  } else {
-    if (value[0] == "error") {
-      console.log(value[1]);
-    }
-
-    var commands = value[1];
-    for (var command_name in commands) {
-      var command = new Command(command_name, commands[command_name]["parameters"]);
-      router.get("/" + command_name, command.doRequest);
-    }
-  }
-});
 
 /*** ****/
 
@@ -131,37 +51,8 @@ var filter = function(row, columns, filters, global) {
   return false;
 }
 
-var CacheControl = function(collection_name) {
-  var self = this;
-  self.collection_name = collection_name;
-  self.data = [];
-  self.counter = -1;
-
-  self.check_status = function () {
-    var client = xmlrpc.createClient(xmlrpc_host);
-    client.methodCall('get_state', [collection_name, "mk8xHba3tqpeRPy4"], function(error, value) {
-      if (error) {
-        console.log('error:', error);
-        console.log('req headers:', error.req && error.req._header);
-        console.log('res code:', error.res && error.res.statusCode);
-        console.log('res body:', error.body);
-      } else {
-        console.log("value: " + value[1]);
-        return value[1] == self.counter;
-      }
-    });
-  }
-
-  self.get_data = function () {
-
-  }
-}
-
-var biosources_cache = new CacheControl("biosources");
-biosources_cache.check_status();
 
 var sort_data = function(data, pos, direction) {
-
   console.log(data);
   data.sort(function(a, b) {
     if (direction == "asc") {
---
+++ rest_to_xmlrpc.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var xmlrpc = require('xmlrpc');
+
+var settings = require('./settings');
+
+var router = express.Router();
+
+xmlrpc_host = settings.xmlrpc_host();
+
+var client = xmlrpc.createClient(xmlrpc_host);
+
+var Command = function(name, parameters) {
+  var self = this;
+  self.name = name;
+  self.parameters = parameters;
+
+  self.convert = function(value, type) {
+    if (type == "string") {
+      return valeue;
+    }
+  }
+
+  self.doRequest = function(req, res) {
+    console.log(self);
+    console.log("executing : " + name + " " + parameters);
+    console.log(req.query);
+
+    var xmlrpc_request_parameters = [];
+
+    for (pos in self.parameters) {
+      var parameter = self.parameters[pos];
+      var parameter_name = parameter[0];
+      var parameter_type = parameter[1];
+      var multiple = parameter[2];
+      console.log(req.query);
+      console.log("param: " + req.query[parameter_name] + " type " + parameter_type);
+      if (parameter_name in req.query) {
+        var raw_value = req.query[parameter_name];
+        if (parameter_type == "string") {
+          xmlrpc_request_parameters.push(raw_value);
+        } else if (parameter_type == "int") {
+          xmlrpc_request_parameters.push(parseInt(raw_value));
+        } else if (parameter_type == "double") {
+          xmlrpc_request_parameters.push(parseFloat(raw_value));
+        } else {
+          res.send("Internal error: Unknown variables type " + parameter_type);
+          return;
+        }
+      } else {
+        xmlrpc_request_parameters.push(null);
+      }
+    }
+    console.log(xmlrpc_request_parameters);
+
+    client.methodCall(name, xmlrpc_request_parameters, function(error, value) {
+      if (error) {
+        console.log('error:', error);
+        console.log('req headers:', error.req && error.req._header);
+        console.log('res code:', error.res && error.res.statusCode);
+        console.log('res body:', error.body);
+      } else {
+        res.send(value);
+      }
+    });
+  }
+}
+
+client.methodCall('commands', [], function(error, value) {
+  if (error) {
+    console.log('error:', error);
+    console.log('req headers:', error.req && error.req._header);
+    console.log('res code:', error.res && error.res.statusCode);
+    console.log('res body:', error.body);
+  } else {
+    if (value[0] == "error") {
+      console.log(value[1]);
+    }
+
+    var commands = value[1];
+    for (var command_name in commands) {
+      var command = new Command(command_name, commands[command_name]["parameters"]);
+      router.get("/" + command_name, command.doRequest);
+    }
+  }
+});
+
+module.exports = {
+  router: router
+}
