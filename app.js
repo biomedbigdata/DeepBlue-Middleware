56bcd497332f3d9486a79d06742dd98b0677304d
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-07-02 19:54:09 +0200

    Do not render errors output

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var session = require('express-session');
 
 var datatable = require('./datatable');
 var rest_to_xmlrpc = require('./rest_to_xmlrpc');
@@ -17,8 +16,6 @@ router.get('/datatable', datatable);
 
 app.use('/', router);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -39,8 +36,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
+    res.status(err.status || 500).send({
       message: err.message,
       error: err
     });
@@ -50,10 +46,9 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+  res.status(err.status || 500).send({
     message: err.message,
-    error: {}
+    error: err
   });
 });
 
