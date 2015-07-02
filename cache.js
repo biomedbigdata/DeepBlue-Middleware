f323bc99f96c6980f0b7ecf0788eba4af3c85413
Felipe Albrecht <felipe.albrecht@gmail.com>
2015-07-02 21:23:25 +0200

    Use user_key to load relevant information

--- cache.js
+++ cache.js
@@ -5,10 +5,7 @@ var utils = require('./utils');
 
 var xmlrpc_host = settings.xmlrpc_host();
 
-var user_key = "hzGoa6a6e62sND4K";
-
-
-var CacheControl = function (collection_name, parameters) {
+var CacheControl = function(collection_name, parameters) {
   console.log("Creating cache controle for " + collection_name);
   var self = this;
   self.collection_name = collection_name;
@@ -18,93 +15,94 @@ var CacheControl = function (collection_name, parameters) {
   self.matches = 0;
   self.requests = 0;
 
-  if (parameters) {
-    self.parameters = parameters.concat([user_key]);
-  } else {
-    self.parameters = [user_key];
-  }
+  self.parameters = parameters;
 
-  self.get = function (callback) {
-    self.requests++;
-    var client = xmlrpc.createClient(xmlrpc_host);
-    client.methodCall('get_state', [collection_name, user_key], function (error, value) {
-      if (error) {
-        callback(error);
-      } else {
-        var counter = value[1];
-        if (value[1] == self.counter) {
-          self.matches++;
-          process.nextTick(function () {
-            callback(error, self.data);
-          });
+  self.get = function(user_key, callback) {
+      self.requests++;
+      var client = xmlrpc.createClient(xmlrpc_host);
+      client.methodCall('get_state', [collection_name, user_key], function(error, value) {
+        if (error) {
+          callback(error);
         } else {
-          self.load_data(counter, callback);
+          var counter = value[1];
+          if (value[1] == self.counter) {
+            self.matches++;
+            process.nextTick(function() {
+              callback(error, self.data);
+            });
+          } else {
+            self.load_data(user_key, counter, callback);
+          }
         }
+      });
+    },
+
+    self.load_data = function(user_key, counter, callback) {
+      var function_name = "list_" + self.collection_name;
+      console.log("load new data");
+
+      if (parameters) {
+        parameters = self.parameters.concat([user_key]);
+      } else {
+        parameters = [user_key];
       }
-    });
-  },
+      var client = xmlrpc.createClient(xmlrpc_host);
+      client.methodCall(function_name, parameters, function(error, value) {
 
-  self.load_data = function (counter, callback) {
-    var function_name = "list_" + self.collection_name;
-    console.log("load new data");
-
-    var client = xmlrpc.createClient(xmlrpc_host);
-    client.methodCall(function_name, self.parameters, function (error, value) {
-
-      if (error) {
-        callback(error);
-      }
-
-      if (value[0] == "error") {
-        console.log(value[1]);
-      }
-      var ids = [];
-      var data = value[1];
-      console.log("processings ids");
-      for (var count in data) {
-        ids.push(data[count][0]);
-      }
-
-      console.log("request info");
-      client.methodCall('info', [ids, user_key], function (error, infos) {
-        console.log(infos);
-        console.log(error);
         if (error) {
           callback(error);
         }
 
-        var infos_data = infos[1];
-
-        console.log("building json");
-        for (var d in infos_data) {
-          if (infos_data[d].type == "experiment") {
-            infos_data[d].extra_metadata = utils.experiments_extra_metadata(infos_data[d]);
-            infos_data[d].biosource = infos_data[d].sample_info.biosource_name;
-          }
-
-          if (infos_data[d].type == "annotation") {
-            infos_data[d].extra_metadata = utils.annotations_extra_metadata(infos_data[d]);
-          }
-
-          if (infos_data[d].type == "biosource") {
-            infos_data[d].extra_metadata = utils.biosources_extra_metadata(infos_data[d]);
-          }
-
-          if (infos_data[d].type == "sample") {
-            infos_data[d].extra_metadata = utils.samples_extra_metadata(infos_data[d]);
-          }
-
-          if (infos_data[d].type == "column_type") {
-            infos_data[d].info = utils.column_type_info(infos_data[d]);
-          }
+        if (value[0] == "error") {
+          console.log(value[1]);
+        }
+        var ids = [];
+        var data = value[1];
+        console.log("processings ids");
+        for (var count in data) {
+          ids.push(data[count][0]);
         }
 
-        self.data = infos[1];
-        self.counter = counter;
-        callback(error, self.data);
+        console.log("request info");
+        client.methodCall('info', [ids, user_key], function(error, infos) {
+          console.log(infos);
+          console.log(error);
+          if (error) {
+            callback(error);
+          }
+
+          var infos_data = infos[1];
+
+          console.log("building json");
+          for (var d in infos_data) {
+            if (infos_data[d].type == "experiment") {
+              infos_data[d].extra_metadata = utils.experiments_extra_metadata(infos_data[d]);
+              infos_data[d].biosource = infos_data[d].sample_info.biosource_name;
+            }
+
+            if (infos_data[d].type == "annotation") {
+              infos_data[d].extra_metadata = utils.annotations_extra_metadata(infos_data[d]);
+            }
+
+            if (infos_data[d].type == "biosource") {
+              infos_data[d].extra_metadata = utils.biosources_extra_metadata(infos_data[d]);
+            }
+
+            if (infos_data[d].type == "sample") {
+              infos_data[d].extra_metadata = utils.samples_extra_metadata(infos_data[d]);
+            }
+
+            if (infos_data[d].type == "column_type") {
+              infos_data[d].info = utils.column_type_info(infos_data[d]);
+            }
+          }
+
+          self.data = infos[1];
+          self.counter = counter;
+          callback(error, self.data);
+        });
       });
-    });
-  };
+    };
 };
 
 var annotations = new CacheControl("annotations", [""]);
