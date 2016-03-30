b7b6e842dbb08a50075ea047d16cb51ee6bc3bd5
Felipe Albrecht <felipe.albrecht@gmail.com>
2016-03-30 08:32:38 +0200

    Formatting

--- users.js
+++ users.js
@@ -10,45 +10,45 @@ var last_check = Date.now();
 var users_info_cache = {};
 
 var check = function (user_key) {
-	var deferred = Q.defer();
-	console.log("users.check");
-	var client = xmlrpc.createClient(xmlrpc_host);
+  var deferred = Q.defer();
+  var client = xmlrpc.createClient(xmlrpc_host);
 
-	// clear the users cache ever minute
-	if (Date.now() > last_check + 60000) {
-		console.log("Clearning users info cache");
-		users_info_cache = {};
-		last_check = Date.now()
-	}
+  // clear the users cache ever minute
+  if (Date.now() > last_check + 60000) {
+    users_info_cache = {};
+    last_check = Date.now()
+  }
 
-	if (user_key in users_info_cache) {
-		deferred.resolve(users_info_cache[user_key]);
-	}
+  if (user_key in users_info_cache) {
+    deferred.resolve(users_info_cache[user_key]);
+  } else {
+    client.methodCall('info', ["me", user_key], function(error, infos) {
+      if (error) {
+        deferred.reject(error);
+        return;
+      }
 
-	client.methodCall('info', ["me", user_key], function(error, infos) {
-		if (error) {
-			deferred.reject(error);
-		}
+      if (infos[0] == "error") {
+        deferred.reject(infos[1]);
+        return;
+      }
 
-		if (infos[0] == "error") {
-			deferred.reject(infos[1]);
-		}
+      var user_info = infos[1][0];
 
-		var user_info = infos[1][0];
+      if (user_info["permission_level"] == "NONE") {
+        deferred.reject({"error":"Invalid permission level."});
+        return;
+      }
 
-		if (user_info["permission_level"] == "NONE") {
-			deferred.reject({"error":"Invalid permission level level"});
-		}
-
-		users_info_cache[user_key] = user_info;
-		deferred.resolve(user_info);
-	});
-
-	return deferred.promise;
+      users_info_cache[user_key] = user_info;
+      deferred.resolve(user_info);
+    });
+  }
+  return deferred.promise;
 }
 
 
 module.exports = {
-	"check" : check
+  "check" : check
 }
 
