bc13d05b4ef45c1ae767082e0e60852032cda9dc
Felipe Albrecht <felipe.albrecht@gmail.com>
2017-08-16 23:05:09 +0900

    reformat

--- src/typescript/app/service/regions_enrichment.ts
+++ src/typescript/app/service/regions_enrichment.ts
@@ -1,7 +1,7 @@
 import { RequestStatus } from '../domain/status';
 import { DeepBlueResult } from '../domain/operations';
-import { DeepBlueService } from "app/service/deepblue";
-import { IdName, IdNameCount } from "app/domain/deepblue";
+import { DeepBlueService } from "../service/deepblue";
+import { IdName, IdNameCount } from "../domain/deepblue";
 import { Observable } from "rxjs/Observable";
 import { Subject } from "rxjs";
 
@@ -16,26 +16,19 @@ export class RegionsEnrichment {
 
   private listExperimentsMany(request_status: RequestStatus, epigenetic_marks: string[]): Observable<Array<[string, string[]]>> {
     let observableBatch: Observable<[string, string[]]>[] = [];
-
-
     epigenetic_marks.forEach((epigenetic_mark: string) => {
       observableBatch.push(this.listExperiments(request_status, epigenetic_mark));
     });
-
     return Observable.forkJoin(observableBatch);
-
   }
 
   private getHistoneModificationDatabases(request_status: RequestStatus, genome: string): Observable<[string, string[]][]> {
     let pollSubject = new Subject<[string, string[]][]>();
 
     this.deepBlueService.collection_experiments_count(request_status, "epigenetic_marks", "peaks", genome).subscribe((ems: IdNameCount[]) => {
-      console.log(ems);
       let histone_marks_names = ems.map((id_name: IdNameCount) => id_name.name);
-      console.log(histone_marks_names);
       this.listExperimentsMany(request_status, histone_marks_names).subscribe((dbs: [string, string[]][]) => {
         pollSubject.next(dbs.filter((em) => {
-          console.log(em[0], em[1].length);
           return em[1].length > 0
         }));
         pollSubject.complete();
@@ -46,12 +39,11 @@ export class RegionsEnrichment {
   }
 
   buildDatabases(request_status: RequestStatus, genome: string): Observable<[string, string[]][]> {
-
+    /*
     "Histone Modification"
     "Transcription Factor Binding Sites"
     "Gene Expression"
-
-    console.log("aaaa");
+    */
     return this.getHistoneModificationDatabases(request_status, genome);
   }
 }
